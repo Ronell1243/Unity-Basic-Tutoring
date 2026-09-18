44abf92865221bb8258bddc172e7e5a836ef0a22
Ronell1243 <mesogem1243@gmail.com>
2026-09-18 17:09:05 +0900

    [Fead] 스크립트 파일 생성

---
+++ My project/Assets/Test.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    
+    void Start()
+    {
+        //int Age
+        //bool 
+        //string Name
+        //float 
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
